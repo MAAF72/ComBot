5cdab93bad25b4f738ab02d5f0553dd27012c0c5
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2020-07-31 09:00:10 +0700

    Cleaning code

--- TlxContest.py
+++ TlxContest.py
@@ -1,14 +1,12 @@
-import discord
-import requests
 from time import time
-from requests import get
 from copy import deepcopy
 from datetime import datetime
-from datetime import timedelta
+from requests import get
+import discord
 
 class TlxContest:
-    def __init__(self, id: int, name: str, admin: discord.member.Member):
-        self.id = id
+    def __init__(self, ID: int, name: str, admin: discord.member.Member):
+        self.ID = ID
         self.name = name
         self.admin = admin
         self.problems = dict()
@@ -18,70 +16,68 @@ class TlxContest:
         self.end = None
         self.scoreboard = dict()
         self.dummy_score = dict()
-        self.procs = []
         self.is_crawling = False
-    
+
     def is_over(self):
-        if self.start != None and datetime.timestamp(datetime.now()) > self.end:
+        if self.start is not None and datetime.timestamp(datetime.now()) > self.end:
             return True
         return False
-    
+
     def set_duration(self, duration):
-        if self.start != None:
+        if self.start is not None:
             return 'Tidak bisa mengatur ulang durasi kontes setelah dimulai'
-            
+
         self.duration = duration
         return 'OK'
-    
+
     def add_duration(self, duration):
         if self.is_over():
             return 'Kontes sudah selesai'
-            
+
         self.duration += duration
         self.end += duration * 60
         return 'OK'
-        
+
     def add_problem(self, slug):
         if slug in self.problems:
             return 'Problem sudah ditambahkan pada kontes ini'
-        if self.start != None:
+        if self.start is not None:
             return 'Tidak bisa menambahkan problem setelah kontes dimulai'
         if self.is_over():
             return 'Kontes sudah selesai'
 
         contest_slug, problem_alias = slug.split('/')
         json_contest = get('https://jerahmeel.tlx.toki.id/api/v2/problemsets/slug/{}'.format(contest_slug)).json()
-        
+
         if 'errorCode' in json_contest:
             return 'Problem tidak ditemukan'
-        
+
         json_problem = get('https://jerahmeel.tlx.toki.id/api/v2/problemsets/{}/problems/{}/worksheet?language=id'.format(json_contest['jid'], problem_alias)).json()
-        
+
         if 'errorCode' in json_problem:
             return 'Problem tidak ditemukan'
-        
+
         self.problems[slug] = {
             'jid': json_problem['problem']['problemJid'],
             'name': json_problem['worksheet']['statement']['title'],
             'latestSubmission': -1
         }
-        
+
         return 'OK'
-        
+
     def del_problem(self, slug):
         if slug not in self.problems:
             return 'Problem tidak ditemukan pada kontes ini'
-        if self.start != None:
+        if self.start is not None:
             return 'Tidak bisa menghapus problem setelah kontes dimulai'
-            
+
         del self.problems[slug]
         return 'OK'
-        
+
     def crawl(self, slug):
         finish = False
         page = 1
         jid = self.problems[slug]['jid']
-        found_pending = False
         stack = []
         while not finish:
             url = 'https://jerahmeel.tlx.toki.id/api/v2/submissions/programming?page={}&problemJid={}'.format(page, jid)
@@ -91,9 +87,9 @@ class TlxContest:
                     if i['id'] == self.problems[slug]['latestSubmission']:
                         finish = True
                         break
-                    
+
                     tlx_username = json_submission['profilesMap'][i['userJid']]['username']
-                    
+
                     if tlx_username in self.players.values():
                         stack.append({
                             'id': i['id'],
@@ -103,36 +99,36 @@ class TlxContest:
                             'username': tlx_username
                         })
                 page += 1
-                
+
         # Process stack
         while stack:
             curr = stack.pop()
             if curr['verdict'] == '?':
                 break
-            
+
             user_scoreboard = self.scoreboard[curr['username']]
             if curr['score'] > user_scoreboard[slug]['score']:
                 user_scoreboard['totalScore'] += curr['score'] - user_scoreboard[slug]['score']
-                user_scoreboard['totalTime'] += (curr['time'] // 1000) - self.start - user_scoreboard[slug]['time'] 
+                user_scoreboard['totalTime'] += (curr['time'] // 1000) - self.start - user_scoreboard[slug]['time']
                 user_scoreboard[slug]['score'] = curr['score']
                 user_scoreboard[slug]['time'] = (curr['time'] // 1000) - self.start
 
             self.problems[slug]['latestSubmission'] = curr['id']
-        
+
     def start_contest(self):
-        if self.start != None:
+        if self.start is not None:
             return 'Kontes sudah dimulai'
         if len(self.problems) == 0:
             return 'Problem belum diatur'
         if self.duration <= 0:
             return 'Durasi kontes belum diatur'
-        
+
         # Create dummy_score
         self.dummy_score = {
             'totalScore': 0,
             'totalTime': 0,
         }
-        for slug in self.problems.keys():
+        for slug in self.problems:
             self.dummy_score[slug] = {
                 'score': 0,
                 'time': 0,
@@ -141,56 +137,55 @@ class TlxContest:
         # Register players biar bisa dicrawl
         for tlx_username in list(self.players.values()):
             self.scoreboard[tlx_username] = deepcopy(self.dummy_score)
-            
+
         self.start = int(datetime.timestamp(datetime.now()))
         self.end = self.start + self.duration * 60
-        
-        #get latestSubmission for every problem  
-        procs = []
-        for slug in self.problems.keys():
+
+        #get latestSubmission for every problem
+        for slug in self.problems:
             url = 'https://jerahmeel.tlx.toki.id/api/v2/submissions/programming?page=1&problemJid={}'.format(self.problems[slug]['jid'])
             json_submission = get(url).json()
             if len(json_submission['data']['page']) > 0:
                 self.problems[slug]['latestSubmission'] = json_submission['data']['page'][0]['id']
         return 'OK'
-        
+
     def add_player(self, user, tlx_username):
         if user in self.players:
-            return 'Sudah terdaftar sebagai'.format(self.players[user])
+            return 'Sudah terdaftar sebagai {}'.format(self.players[user])
         if self.is_over():
             return 'Kontes sudah selesai'
 
         json_user = get('https://jerahmeel.tlx.toki.id/api/v2/user-stats?username={}'.format(tlx_username)).json()
-        
+
         if 'errorCode' in json_user:
             return 'Akun TLX {} tidak dapat ditemukan'.format(tlx_username)
-        
+
         # Join di tengah2, masukkan datanya juga ke scores dan submission agar bisa kedetect pas crawling
         self.players[user] = tlx_username
-        if self.start != None:
+        if self.start is not None:
             self.scoreboard[tlx_username] = deepcopy(self.dummy_score)
 
         return 'OK'
-    
+
     def del_player(self, user):
         if user not in self.players:
             return 'Tidak terdaftar pada kontes'
-        if self.start != None:
+        if self.start is not None:
             return 'Tidak bisa mengundurkan diri setelah kontes dimulai'
         if self.is_over():
             return 'Kontes sudah selesai'
-        
+
         del self.players[user]
         return 'OK'
-        
+
     def update_scoreboard(self):
         if self.is_crawling:
             return 'Mohon tunggu, proses sebelumnya belum selesai...'
 
         self.is_crawling = True
         start_time = time()
-        for slug in self.problems.keys():
+        for slug in self.problems:
             self.crawl(slug)
         print('Successfully crawling {} problem(s) in {:.2f}s'.format(len(self.problems), time() - start_time))
-        self.is_crawling = False    
+        self.is_crawling = False
         return 'OK'
--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-import discord
-from pytz import timezone
-from TlxContest import TlxContest
-from discord.ext import commands
 from datetime import datetime
+from pytz import timezone
+import discord
+from TlxContest import TlxContest
+
 
 ONE = 'NzM1NzEyNTY1NDU1MzU1OTA2'
 TWO = 'XxkPuA'
@@ -11,56 +11,60 @@ TOKEN = ONE + '.' + TWO + '.' + THREE
 
 
 COLOR = {
-    'danger': 0xdc3545,
-    'info': 0x17a2b8,
-    'warning': 0xffc107,
-    'success': 0x28a745
+    'danger': 0xDC3545,
+    'info': 0x17A2B8,
+    'warning': 0xFFC107,
+    'success': 0x28A745
 }
 
-bot = commands.Bot(command_prefix=';')
+bot = discord.ext.commands.Bot(command_prefix=';')
 
 bot.tlx_contests = dict()
 bot.auto_increment = 1
 
-def generate_contest_embed(contest):
+def generate_contest_embed(contest, sender):
     problem_value = '-------'
     durasi_value = '------'
-    
+
     if len(contest.problems) > 0:
-        problem_value = '\n'.join('- [{}](https://tlx.toki.id/problems/{})'.format(contest.problems[p]['name'], p) for p in contest.problems.keys())
-        
+        if sender is contest.admin or contest.start is not None:
+            problem_value = '\n'.join('- [{}](https://tlx.toki.id/problems/{})'.format(contest.problems[p]['name'], p) for p in contest.problems)
+        else:
+            problem_value = '- ?????\n' * (len(contest.problems) - 1) + '- ?????'
+
     if contest.duration > 0:
         durasi_value = '{} menit'.format(contest.duration)
-        if contest.start != None:
+        if contest.start is not None:
             durasi_value += '\n ({} - {})'.format(datetime.fromtimestamp(contest.start, timezone('Asia/Jakarta')).strftime('%d %B %Y %H:%M'), datetime.fromtimestamp(contest.end, timezone('Asia/Jakarta')).strftime('%d %B %Y %H:%M'))
-    
-    embed = discord.Embed(title=' ', description='Author: {} | Contest ID: {}'.format(contest.admin.mention, contest.id), color=COLOR['info'])
+
+    embed = discord.Embed(title=' ', description='Author: {} | Contest ID: {}'.format(contest.admin.mention, contest.ID), color=COLOR['info'])
     embed.set_author(name=contest.name)
-    
+
     embed.add_field(name='Problem', value=problem_value, inline=True)
     embed.add_field(name='Durasi', value=durasi_value, inline=True)
-    if contest.start == None:
+    if contest.start is None:
         embed.set_footer(text='Kontes belum dimulai')
     elif contest.is_over():
         embed.set_footer(text='Kontes sudah selesai')
     else:
         embed.set_footer(text='Kontes akan selesai dalam {} menit, happy coding!'.format((contest.end - int(datetime.timestamp(datetime.now(timezone('Asia/Jakarta'))))) // 60))
+    # embed.timestamp = datetime.now(timezone('Asia/Jakarta'))
 
     return embed
 
 def generate_scoreboard_embed(contest):
     rank_value = ''
     score_value = ''
-    
+
     sort_orders = sorted(contest.scoreboard.items(), key=lambda x: (x[1]['totalScore'], -x[1]['totalTime']), reverse=True)
     for index, item in enumerate(sort_orders, start=1):
-        for user in contest.players.keys():
+        for user in contest.players:
             if contest.players[user] == item[0]:
                 rank_value += '`{}` {} ({})\n'.format(index, user.mention, item[0])
                 score_value += '`{}`  `{}`\n'.format(item[1]['totalScore'], item[1]['totalTime'] // 60)
                 break
-    
-    embed = discord.Embed(title=' ', description='Author: {} | Contest ID: {}'.format(contest.admin.mention, contest.id), color=COLOR['info'])
+
+    embed = discord.Embed(title=' ', description='Author: {} | Contest ID: {}'.format(contest.admin.mention, contest.ID), color=COLOR['info'])
     embed.set_author(name='Scoreboard {}'.format(contest.name))
     embed.add_field(name='Rank', value=rank_value, inline=True)
     embed.add_field(name='Score', value=score_value, inline=True)
@@ -68,63 +72,66 @@ def generate_scoreboard_embed(contest):
         embed.set_footer(text='Kontes sudah selesai')
     else:
         embed.set_footer(text='Kontes akan selesai dalam {} menit, happy coding!'.format((contest.end - int(datetime.timestamp(datetime.now(timezone('Asia/Jakarta'))))) // 60))
+    # embed.timestamp = datetime.now(timezone('Asia/Jakarta'))
 
     return embed
 
 async def embed_message(ctx, message, level):
-    await ctx.send(embed=discord.Embed(description='{} {}'.format(message, ctx.message.author.mention), color=COLOR[level]))
-    
+    return await ctx.send(embed=discord.Embed(description='{} {}'.format(message, ctx.message.author.mention), color=COLOR[level]))
+
 @bot.event
 async def on_ready():
     app_info = await bot.application_info()
     await app_info.owner.send('{} has connected to Discord!'.format(bot.user.name))
-    
+
 @bot.event
 async def on_error(event, *args, **kwargs):
-    with open('error.log', 'a') as f:
-        f.writelines('Unhandled message: {}'.format(args))
-        
+    with open('error.log', 'a') as log_file:
+        log_file.writelines('Event:', event)
+        log_file.writelines('Unhandled message:', args)
+        log_file.writelines('Kwargs:', kwargs)
+
 @bot.command(name='tlx_contest', help='Menampilkan kontes TLX')
 async def tlx_contest_show(ctx, contest_id: int):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-    
+
     contest = bot.tlx_contests[contest_id]
-    await ctx.send(embed=generate_contest_embed(contest))
-        
+    await ctx.send(embed=generate_contest_embed(contest, ctx.author))
+
 @bot.command(name='tlx_contest+', help='Membuat kontes TLX')
 async def tlx_contest_create(ctx, *, name: str):
     contest = TlxContest(bot.auto_increment, name, ctx.message.author)
     bot.auto_increment += 1
-    bot.tlx_contests[contest.id] = contest
-    
-    await ctx.send(embed=generate_contest_embed(contest))
-    
+    bot.tlx_contests[contest.ID] = contest
+
+    await ctx.send(embed=generate_contest_embed(contest, ctx.author))
+
 @bot.command(name='tlx_contest-', help='Menghapus kontes TLX')
 async def tlx_contest_delete(ctx, contest_id: int):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-    
+
     contest = bot.tlx_contests[contest_id]
-    
+
     if contest.admin != ctx.message.author:
         await embed_message(ctx, 'Perintah ditolak, Anda tidak memiliki akses', 'danger')
         return
-    
+
     del bot.tlx_contests[contest_id]
-    
+
     await embed_message(ctx, 'Kontes berhasil dihapus', 'success')
-    
+
 @bot.command(name='tlx_problem+', help='Menambahkan problem ke kontes TLX')
 async def tlx_problem_add(ctx, contest_id: int, slug: str):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-        
+
     contest = bot.tlx_contests[contest_id]
-    
+
     if contest.admin != ctx.message.author:
         await embed_message(ctx, 'Perintah ditolak, Anda tidak memiliki akses', 'danger')
         return
@@ -135,51 +142,51 @@ async def tlx_problem_add(ctx, contest_id: int, slug: str):
         return
 
     await embed_message(ctx, 'Problem berhasil ditambahkan', 'success')
-    
+
 @bot.command(name='tlx_problem-', help='Menghapus problem dari kontes TLX')
 async def tlx_problem_delete(ctx, contest_id: int, slug: str):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-    
+
     contest = bot.tlx_contests[contest_id]
-    
+
     if contest.admin != ctx.message.author:
         await embed_message(ctx, 'Perintah ditolak, Anda tidak memiliki akses', 'danger')
         return
-    
+
     response = contest.del_problem(slug)
     if response != 'OK':
         await embed_message(ctx, response, 'warning')
         return
 
     await embed_message(ctx, 'Problem berhasil dihapus', 'success')
-    
+
 @bot.command(name='tlx_duration', help='Mengatur durasi(menit) kontes TLX')
 async def tlx_duration_set(ctx, contest_id: int, duration: int):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-    
+
     contest = bot.tlx_contests[contest_id]
-    
+
     if contest.admin != ctx.message.author:
         await embed_message(ctx, 'Perintah ditolak, Anda tidak memiliki akses', 'danger')
         return
-        
+
     response = contest.set_duration(duration)
     if response != 'OK':
         await embed_message(ctx, response, 'warning')
         return
 
     await embed_message(ctx, 'Durasi kontes berhasil diatur', 'success')
-    
+
 @bot.command(name='tlx_player+', help='Mendaftarkan diri ke kontes TLX')
 async def tlx_player_add(ctx, contest_id: int, tlx_username: str):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-        
+
     contest = bot.tlx_contests[contest_id]
 
     response = contest.add_player(ctx.message.author, tlx_username)
@@ -188,13 +195,13 @@ async def tlx_player_add(ctx, contest_id: int, tlx_username: str):
         return
 
     await embed_message(ctx, 'Berhasil mendaftar', 'success')
-    
+
 @bot.command(name='tlx_player-', help='Mengundurkan diri dari kontes TLX')
 async def tlx_player_del(ctx, contest_id: int):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-        
+
     contest = bot.tlx_contests[contest_id]
 
     response = contest.del_player(ctx.message.author)
@@ -209,73 +216,74 @@ async def tlx_start(ctx, contest_id: int):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-    
+
     contest = bot.tlx_contests[contest_id]
-    
+
     if contest.admin != ctx.message.author:
         await embed_message(ctx, 'Perintah ditolak, Anda tidak memiliki akses', 'danger')
         return
-    
+
     wait_message = await embed_message(ctx, 'Mohon tunggu, sedang menyiapkan kontes...', 'info')
     response = contest.start_contest()
     await wait_message.delete()
     if response != 'OK':
         await embed_message(ctx, response, 'warning')
         return
-    
+
     message = ''
     message += '`{} dimulai!`\n'.format(contest.name)
     message += ' '.join('{}'.format(p.mention) for p in contest.players)
-    await ctx.send(embed=generate_contest_embed(contest))
+    await ctx.send(embed=generate_contest_embed(contest, ctx.author))
     if len(contest.players) > 0:
         await ctx.send(embed=generate_scoreboard_embed(contest))
 
     await ctx.send(message)
-    
+
 @bot.command(name='tlx_scoreboard', help='Menampilkan scoreboard kontes TLX')
 async def tlx_scoreboard(ctx, contest_id: int):
     if contest_id not in bot.tlx_contests:
         await embed_message(ctx, 'Kontes tidak ditemukan', 'warning')
         return
-    
+
     contest = bot.tlx_contests[contest_id]
-    
-    if contest.start == None:
+
+    if contest.start is None:
         await embed_message(ctx, 'Kontes belum dimulai', 'warning')
         return
-    
+
     if len(contest.players) == 0:
         await embed_message(ctx, 'Peserta kontes tidak ada, tidak bisa menampilkan scoreboard', 'warning')
         return
-        
+
     wait_message = await embed_message(ctx, 'Mohon tunggu, sedang menyiapkan scoreboard...', 'info')
-        
+
     response = contest.update_scoreboard()
     await wait_message.delete()
     if response != 'OK':
         await embed_message(ctx, response, 'warning')
         return
-    
+
     await ctx.send(embed=generate_scoreboard_embed(contest))
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        await embed_message(ctx, 'You do not have the correct role for this command.')
-    elif isinstance(error, commands.CommandInvokeError):
+    if isinstance(error, discord.ext.commands.CheckFailure):
+        await embed_message(ctx, 'You do not have the correct role for this command.', 'danger')
+    elif isinstance(error, discord.ext.commands.CommandInvokeError):
         await embed_message(ctx, 'Internal Error', 'danger')
+        print(type(error))
         print(error.original)
         embed = discord.Embed(title='ComBot Command Error', color=COLOR['danger'])
-        embed.add_field(name='Error', value=error)
-        embed.add_field(name='Guild', value=ctx.guild)
-        embed.add_field(name='Channel', value=ctx.channel)
-        embed.add_field(name='User', value=ctx.author)
-        embed.add_field(name='Message', value=ctx.message.clean_content)
-        embed.timestamp = datetime.now(timezone('Asia/Jakarta'))
+        embed.add_field(name='Guild', value=ctx.guild, inline=True)
+        embed.add_field(name='Channel', value=ctx.channel, inline=True)
+        embed.add_field(name='User', value=ctx.author, inline=True)
+        embed.add_field(name='Message', value=ctx.message.clean_content, inline=False)
+        embed.add_field(name='Error', value=error, inline=False)
+        # embed.timestamp = datetime.now(timezone('Asia/Jakarta'))
         try:
             app_info = await bot.application_info()
             await app_info.owner.send(embed=embed)
-        except:
+        except: # pylint: disable=bare-except
             pass
 
 bot.run(TOKEN)
